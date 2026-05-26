0ed1673122baf51deb6c9c34b0b8489ac15ac592
JairoNP300 <Zetino19ztn@gmail.com>
2026-05-26 13:34:40 -0600

    Actualización automática: 26/5/2026, 1:34:40 p.m.

--- scratch/check_1620.cjs
+++ scratch/check_1620.cjs
@@ -1,8 +1,8 @@
 const Database = require('better-sqlite3');
 const db = new Database('inventario_oficial.db');
 
-const movements = db.prepare("SELECT * FROM movements WHERE product_id = 412").all();
-console.log('Movements count:', movements.length);
-console.log(JSON.stringify(movements, null, 2));
+const logs = db.prepare("SELECT * FROM activity_log WHERE details LIKE '%1620%' OR product_name LIKE '%1620%'").all();
+console.log('Activity log count:', logs.length);
+console.log(JSON.stringify(logs, null, 2));
 
 db.close();
