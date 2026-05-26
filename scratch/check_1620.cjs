ea0668601e78a66c55477bda8cdf153ce02fa82a
JairoNP300 <Zetino19ztn@gmail.com>
2026-05-26 13:34:29 -0600

    Actualización automática: 26/5/2026, 1:34:29 p.m.

--- scratch/check_1620.cjs
+++ scratch/check_1620.cjs
@@ -1,8 +1,8 @@
 const Database = require('better-sqlite3');
 const db = new Database('inventario_oficial.db');
 
-const prodLogs = db.prepare("SELECT * FROM production_logs WHERE product_id = 412").all();
-console.log('Production logs count:', prodLogs.length);
-console.log(JSON.stringify(prodLogs, null, 2));
+const movements = db.prepare("SELECT * FROM movements WHERE product_id = 412").all();
+console.log('Movements count:', movements.length);
+console.log(JSON.stringify(movements, null, 2));
 
 db.close();
