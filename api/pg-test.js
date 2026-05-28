4459655de59278feb081ca52c33d68de7b4cfdef
JairoNP300 <Zetino19ztn@gmail.com>
2026-05-28 08:20:51 -0600

    Cleanup: remover archivos de prueba

--- api/pg-test.js
+++
@@ -1,8 +0,0 @@
-export default async (req, res) => {
-  try {
-    const pg = await import('pg');
-    res.json({ ok: true, hasPool: typeof pg.Pool, defaultType: typeof pg.default });
-  } catch (e) {
-    res.json({ error: e.message, stack: e.stack?.split('\n')[0] });
-  }
-};
--- api/test-env.js
+++
@@ -1,9 +0,0 @@
-export default (req, res) => {
-  res.json({
-    vercel: !!process.env.VERCEL,
-    vercelEnv: process.env.VERCEL_ENV || '',
-    hasDbUrl: !!process.env.DATABASE_URL,
-    nodeVersion: process.version,
-    cwd: process.cwd()
-  });
-};
